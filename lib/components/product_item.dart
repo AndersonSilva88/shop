8ce878822e1b63fd9443db84cc59d4344eb8c9e4
AndersonOSilva <anderson.os88@hotmail.com>
2025-06-14 06:31:32 -0300

    pastas components, data e pages

---
+++ lib/components/product_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(product.imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+          title: Text(product.title, textAlign: TextAlign.center),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+        ),
+      ),
+    );
+  }
+}
